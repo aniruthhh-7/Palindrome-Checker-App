f2b651b2644c413ac647e07773cc687d5c74952f
aniruthhh-7 <torque.torricelli@gmail.com>
2026-03-04 18:05:54 +0530

    UC10 Completed

---
+++ UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "A man a plan a canal Panama";
+
+        // Normalize string
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+    }
+}
