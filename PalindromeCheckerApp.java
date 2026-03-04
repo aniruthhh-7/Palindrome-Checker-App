c338390557a6a372e5249d9d61cd0a1c92f4440d
aniruthhh-7 <torque.torricelli@gmail.com>
2026-03-04 13:22:18 +0530

    UC2

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version: 1.0.0");
-        System.out.println("System Initialized Successfully.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String input = sc.nextLine();
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Is it a palindrome? : " + isPalindrome);
     }
 }
